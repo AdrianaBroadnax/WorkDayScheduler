89b0548eb42310bc1df7cf8b0ddc774c352dd3e8
Adriana Broadnax <alwaysadrianna@gmail.com>
2021-10-28 17:56:04 -0400

    js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // const taskList = localStorage.getItem('taskList');
 function myFunction(){
-    var taskListToSave={am9:"I eat breakfast", am10:"Meeting with supervisor", am11:"Look for overdue tasks", am8:"taskList8"}
+    var taskListToSave={type:"taskList8"}
     var taskListReadyToSave=JSON.stringify(taskListToSave)
     localStorage.setItem("taskList", taskListReadyToSave)
 }
