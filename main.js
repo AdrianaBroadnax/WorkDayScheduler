c9b0f70cb25647b5b8b330d10417791cefe0f9ce
Adriana Broadnax <alwaysadrianna@gmail.com>
2021-10-28 17:25:19 -0400

    java html css

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-const taskList = localStorage.getItem('taskList');
+// const taskList = localStorage.getItem('taskList');
+function myFunction(){
+    var taskListToSave={am9:"I eat breakfast", am10:"Meeting with supervisor", am11:"Look for overdue tasks"}
+    var taskListReadyToSave=JSON.stringify(taskListToSave)
+    localStorage.setItem("taskList", taskListReadyToSave)
+}
+document.getElementById("saveBtn").onclick = function() {myFunction()};
