eeb765acfdc2feff38b7c3d58a817497354f9660
Adriana Broadnax <alwaysadrianna@gmail.com>
2021-11-04 14:18:02 -0400

    main.js and html

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 // const taskList = localStorage.getItem('taskList');
-function myFunction(){
-    var taskListToSave={type:"taskList"}
-    var taskListReadyToSave=JSON.stringify(taskListToSave)
-    localStorage.setItem("taskList", taskListReadyToSave)
-}
-document.getElementById("saveBtn").onclick = function() {myFunction()};
+// function myFunction(){
+//     var taskListToSave={type:"taskList"}
+//     var taskListReadyToSave=JSON.stringify(taskListToSave)
+//     localStorage.setItem("taskList", taskListReadyToSave)
+// }
+// document.getElementById("saveBtn").onclick = function() {myFunction()};
+
+var saveTaskContainer = document.querySelector("#saveTask-container");
+var saveBtn = document.querySelector("#save-button");
+var clearSaveTask = document.querySelector("#clearAll");
+
+// click event to save local storage 
+saveBtn.addEventListener("click", function () {
+    console.log("save");
+    document.location = "index.html";
+});
+
+// const taskList = localStorage.getItem('taskList');
+// function myFunction(){
+//     var taskListToSave={type:"taskList"}
+//     var taskListReadyToSave=JSON.stringify(taskListToSave)
+//     localStorage.setItem("taskList", taskListReadyToSave)
+// }
+// document.getElementById("saveBtn").onclick = function() {myFunction()};
+
+
+// retrieving saved items from local storage
